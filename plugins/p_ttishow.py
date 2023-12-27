b7ce59f9f6c656a50221f6ef47a75577288175d3
Bebsti <129532033+Bebsti@users.noreply.github.com>
2023-12-27 14:36:18 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -61,11 +61,9 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                                                                           InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                                                                        ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Delta_Owner")
-                                                                         ]]
+                                                                           InlineKeyboardButton('✘ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ✘', url=GRP_LNK),
+                                                                           InlineKeyboardButton('✘ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✘', url=CHNL_LNK)
+                                                                        ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
                 )
@@ -89,10 +87,8 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support Group', url="https://t.me/DeltaFiles"),
-            InlineKeyboardButton('Owner', url="t.me/Delta_Owner")
-        ],[
-            InlineKeyboardButton('Use Me Here', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('✘ WWM ✘', url="https://t.me/World_wide_movies"),
+            InlineKeyboardButton('✘ Use Me Here ✘', url=f'https://t.me/{SUPPORT_CHAT}')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
