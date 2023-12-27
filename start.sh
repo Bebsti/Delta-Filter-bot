0bbfe88465358d294bf0c8a2ef2a140a3ef7fbfa
Bebsti <129532033+Bebsti@users.noreply.github.com>
2023-12-27 13:16:41 +0530

    Update start.sh
    gitchange

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SamarITS/Rocorr.git /Rocorr
+  git clone https://github.com/Bebsti/Delta-Filter-bot.git /Rocorr
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Rocorr
